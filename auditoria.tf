2f3c3bd01227a75ec72005bc630de2d934c30b21
leo rodriguez <leo@MacBook-Air-de-leo.local>
2026-01-28 12:18:55 +0100

    cambios drasticos

--- auditoria.tf
+++ auditoria.tf
@@ -1,33 +1,13 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
+# 🎯 Master and Commander - Configuración de Auditoría
 resource "aws_instance" "auditoria" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-
+  
   subnet_id = data.aws_subnets.default.ids[0]
-
+  
   iam_instance_profile = "LabInstanceProfile"
   vpc_security_group_ids = [aws_security_group.auditoria_sg.id]
-
+  
   tags = {
     proyecto = "cybersec"
     funcion  = "auditoria"
@@ -38,24 +18,24 @@ resource "aws_security_group" "auditoria_sg" {
   name        = "${var.prefix}-auditoria-sg"
   description = "Security Group para instancia de auditoria"
   vpc_id      = data.aws_vpc.default.id
-ingress {
+  
+  ingress {
     description = "SSH desde cualquier IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   tags = {
     proyecto = "cybersec"
     funcion  = "auditoria"
   }
 }
-
